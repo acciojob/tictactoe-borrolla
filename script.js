0ab0163d5b509037d2d83ac2f5212eb48368ff4a
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-04-03 12:45:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
  document.getElementById('submit').addEventListener('click', function() {
-            const player1 = document.getElementById('player-1').value;
-            const player2 = document.getElementById('player-2').value;
+            const player1 = document.getElementById('#player-1').value;
+            const player2 = document.getElementById('#player-2').value;
             
             if (player1 && player2) {
                 document.querySelector('h1').style.display = 'block';
@@ -81,3 +81,5 @@
                 }
             });
         }
+
+
